8ef8914b3e7ce0d9c6a362a178d5cf84d9e69d09
Jakub Szostak <s33162@pjwstk.edu.pl>
2026-06-02 22:28:31 +0200

    Finish

--- DBFirst/Program.cs
+++ DBFirst/Program.cs
@@ -1,3 +1,7 @@
+using DBFirst.Infrastructure;
+using DBFirst.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IHospitalService, HospitalService>();
+
+builder.Services.AddDbContext<HospitalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(opt =>  opt.SwaggerEndpoint("/openapi/v1.json", "My API V1"));
 }
 
 app.UseHttpsRedirection();
